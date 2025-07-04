5e22cbea0df7eba19557328d01dfc637a1894751
Tom.L <tom.louveau@edu.devinci.fr>
2025-07-04 15:40:40 +0200

    ADD chara sprite

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,7 +3,10 @@ package entity;
 import main.GamePanel;
 import main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity {
     GamePanel gp;
@@ -14,31 +17,64 @@ public class Player extends Entity {
         this.kh = kh;
 
         setDefaultValues();
+        getPlayerSprite();
     }
 
     public void setDefaultValues() {
         x = 100;
         y = 100;
         speed = 4;
+        direction = "right";
+    }
+
+    public void getPlayerSprite() {
+        try {
+            right = ImageIO.read(getClass().getResourceAsStream("/images/right.png"));
+            left = ImageIO.read(getClass().getResourceAsStream("/images/left.png"));
+            up = ImageIO.read(getClass().getResourceAsStream("/images/up.png"));
+            down = ImageIO.read(getClass().getResourceAsStream("/images/down.png"));
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void update() {
         if(kh.upPressed) {
+            direction = "up";
             y -= speed;
         }
         if(kh.downPressed) {
+            direction = "down";
             y += speed;
         }
         if(kh.leftPressed) {
+            direction = "left";
             x -= speed;
         }
         if(kh.rightPressed) {
+            direction = "right";
             x += speed;
         }
     }
 
     public void draw(Graphics2D g2) {
-        g2.setColor(Color.white);
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image = null;
+
+        switch (direction) {
+            case "right":
+                image = right;
+                break;
+            case "left":
+                image = left;
+                break;
+            case "up":
+                image = up;
+                break;
+            case "down":
+                image = down;
+                break;
+        }
+
+        g2.drawImage(image, x, y, gp.tileSize,  gp.tileSize, null);
     }
 }
